c368618605c1fca4a98ed783c2b267721124f8ce
Just-a-Unity-Dev <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2024-03-23 22:51:29 +0800

    Viewcones

---
+++ Content.Client/_FTL/Viewcones/ViewconesSystem.cs
@@ -0,0 +1,12 @@
+namespace Content.Client._FTL.Viewcones;
+
+public sealed class ViewconesSystem : EntitySystem
+{
+    public const string ViewconeEntityId = "Viewcone";
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+    }
+}
