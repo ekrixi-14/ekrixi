5ca835c2464f3a90bc31716c12ffc85beb333731
Just-a-Unity-Dev <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2024-03-29 10:59:46 +0800

    Fix tests failing due to printer paper

--- Content.Server/_FTL/PrinterPaper/PrinterPaperSystem.cs
+++ Content.Server/_FTL/PrinterPaper/PrinterPaperSystem.cs
@@ -9,13 +9,6 @@ public sealed class PrinterPaperSystem : EntitySystem
 {
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
-
-    /// <inheritdoc/>
-    public override void Initialize()
-    {
-        SubscribeLocalEvent<RandomPrinterPaperComponent, ComponentInit>(OnInit);
-    }
-
     public override void Update(float frameTime)
     {
         var query = EntityQueryEnumerator<RandomPrinterPaperComponent, PaperComponent>();
@@ -25,14 +18,5 @@ public sealed class PrinterPaperSystem : EntitySystem
         }
 
         base.Update(frameTime);
-    }
-
-    private void OnInit(EntityUid uid, RandomPrinterPaperComponent component, ComponentInit args)
-    {
-        var paper = EnsureComp<PaperComponent>(uid);
-        var prototypes = _prototypeManager.EnumeratePrototypes<PrinterPaperPrototype>().ToList();
-        var content = _random.Pick(prototypes).Content;
-        paper.Content = content;
-        component.Content = content;
     }
 }
