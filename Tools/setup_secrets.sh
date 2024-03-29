702c389b78720647dfab87043a3fe0d914efbee9
Just-a-Unity-Dev <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2024-03-29 21:11:32 +0800

    Setup secrets on linux

---
+++ Tools/setup_secrets.sh
@@ -0,0 +1,6 @@
+git submodule add -f https://github.com/ekrixi-14/secrets Secrets
+ln Secrets/ServerCode Content.ServerSecret/Code
+ln Secrets/SecretResources/Prototypes Resources/Prototypes/SecretPrototypes
+ln Secrets/SecretResources/Textures Resources/Textures/SecretTextures
+
+echo Done.
