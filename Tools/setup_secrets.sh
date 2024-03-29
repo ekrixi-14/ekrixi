0ebb6a108fc6275630f6419fd06170e2a9f5c2ea
Just-a-Unity-Dev <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2024-03-29 21:14:08 +0800

    Fix secrets sh

--- Tools/setup_secrets.sh
+++ Tools/setup_secrets.sh
@@ -1,6 +1,6 @@
-git submodule add -f https://github.com/ekrixi-14/secrets Secrets
-ln Secrets/ServerCode Content.ServerSecret/Code
-ln Secrets/SecretResources/Prototypes Resources/Prototypes/SecretPrototypes
-ln Secrets/SecretResources/Textures Resources/Textures/SecretTextures
+git submodule add -f git@github.com:ekrixi-14/secrets.git Secrets
+ln -s Secrets/ServerCode Content.ServerSecret/Code
+ln -s Secrets/SecretResources/Prototypes Resources/Prototypes/SecretPrototypes
+ln -s Secrets/SecretResources/Textures Resources/Textures/SecretTextures
 
 echo Done.
