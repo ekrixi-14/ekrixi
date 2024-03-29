a11558060992a963c92526bec57a48040ac91ea0
Just-a-Unity-Dev <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2024-03-29 17:34:11 +0800

    Secret textures

--- Tools/setup_secrets.ps1
+++ Tools/setup_secrets.ps1
@@ -1,4 +1,5 @@
 git submodule add -f https://github.com/ekrixi-14/secrets Secrets
 New-Item -ItemType SymbolicLink -Path "Content.ServerSecret\Code" -Target "Secrets\ServerCode"
 New-Item -ItemType SymbolicLink -Path "Resources\Prototypes\SecretPrototypes" -Target "Secrets\SecretResources\Prototypes"
+New-Item -ItemType SymbolicLink -Path "Resources\Textures\SecretTextures" -Target "Secrets\SecretResources\Textures"
 pause
