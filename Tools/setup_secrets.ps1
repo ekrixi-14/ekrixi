4b2c7d1a36735617793a16ec37257d15e9a22912
Just-a-Unity-Dev <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2024-03-31 15:23:38 +0800

    Update how secrets work

--- Tools/setup_secrets.ps1
+++ Tools/setup_secrets.ps1
@@ -1,5 +1,6 @@
 git submodule add -f https://github.com/ekrixi-14/secrets Secrets
-New-Item -ItemType SymbolicLink -Path "Content.ServerSecret\Code" -Target "Secrets\ServerCode"
+New-Item -ItemType SymbolicLink -Path "Content.Server\_SecretCode" -Target "Secrets\ServerCode"
 New-Item -ItemType SymbolicLink -Path "Resources\Prototypes\SecretPrototypes" -Target "Secrets\SecretResources\Prototypes"
+New-Item -ItemType SymbolicLink -Path "Resources\IgnoredPrototypes\secret.yml" -Target "Secrets\ignoredPrototypes.yml"
 New-Item -ItemType SymbolicLink -Path "Resources\Textures\SecretTextures" -Target "Secrets\SecretResources\Textures"
 pause
--- Tools/setup_secrets.sh
+++ Tools/setup_secrets.sh
@@ -1,6 +1,7 @@
 git submodule add -f git@github.com:ekrixi-14/secrets.git Secrets
-ln -s ./Secrets/ServerCode ./Content.ServerSecret/Code
+ln -s ./Secrets/ServerCode ./Content.Server/_SecretCode
 ln -s ./Secrets/SecretResources/Prototypes ./Resources/Prototypes/SecretPrototypes
+ln -s ./Secrets/ignoredPrototypes.yml ./Resources/IgnoredPrototypes/secret.yml
 ln -s ./Secrets/SecretResources/Textures ./Resources/Textures/SecretTextures
 
 echo Done.
