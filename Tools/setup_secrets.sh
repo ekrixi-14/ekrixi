ef640631b5f6e40076aa66c18add197fdc4b4043
Just-a-Unity-Dev <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2024-03-31 20:44:54 +0800

    Update secrets again

--- Tools/setup_secrets.sh
+++ Tools/setup_secrets.sh
@@ -1,7 +1,7 @@
 git submodule add -f git@github.com:ekrixi-14/secrets.git Secrets
-ln -s ./Secrets/ServerCode ./Content.Server/_SecretCode
-ln -s ./Secrets/SecretResources/Prototypes ./Resources/Prototypes/SecretPrototypes
-ln -s ./Secrets/ignoredPrototypes.yml ./Resources/IgnoredPrototypes/secret.yml
-ln -s ./Secrets/SecretResources/Textures ./Resources/Textures/SecretTextures
+ln -sf $PWD/Secrets/ServerCode/ $PWD/Content.Server/_SecretCode
+ln -sf $PWD/Secrets/SecretResources/Prototypes $PWD/Resources/Prototypes/SecretPrototypes
+ln -sf $PWD/Secrets/ignoredPrototypes.yml $PWD/Resources/IgnoredPrototypes/secret.yml
+ln -sf $PWD/Secrets/SecretResources/Textures $PWD/Resources/Textures/SecretTextures
 
 echo Done.
