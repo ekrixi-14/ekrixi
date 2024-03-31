e7920c402e08b2835a955163ee587ade20acecb7
Just-a-Unity-Dev <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2024-03-31 14:12:16 +0800

    Fix secrets (#234)
    Fixes Content.ServerSecret and fixes the linking on setup_secrets.sh

--- Tools/setup_secrets.sh
+++ Tools/setup_secrets.sh
@@ -1,6 +1,6 @@
 git submodule add -f git@github.com:ekrixi-14/secrets.git Secrets
-ln -s Secrets/ServerCode Content.ServerSecret/Code
-ln -s Secrets/SecretResources/Prototypes Resources/Prototypes/SecretPrototypes
-ln -s Secrets/SecretResources/Textures Resources/Textures/SecretTextures
+ln -s ./Secrets/ServerCode ./Content.ServerSecret/Code
+ln -s ./Secrets/SecretResources/Prototypes ./Resources/Prototypes/SecretPrototypes
+ln -s ./Secrets/SecretResources/Textures ./Resources/Textures/SecretTextures
 
 echo Done.
