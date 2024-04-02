072db93a0ac92c406945f1d382997e5275cf76c2
Just-a-Unity-Dev <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2024-04-02 16:33:43 +0800

    Do not link ignored prototypes temporarily

--- Tools/setup_secrets.sh
+++ Tools/setup_secrets.sh
@@ -1,7 +1,7 @@
 git submodule add -f git@github.com:ekrixi-14/secrets.git Secrets
 ln -sf $PWD/Secrets/ServerCode/ $PWD/Content.Server/_SecretCode
 ln -sf $PWD/Secrets/SecretResources/Prototypes $PWD/Resources/Prototypes/SecretPrototypes
-ln -sf $PWD/Secrets/ignoredPrototypes.yml $PWD/Resources/IgnoredPrototypes/secret.yml
+#ln -sf $PWD/Secrets/ignoredPrototypes.yml $PWD/Resources/IgnoredPrototypes/secret.yml
 ln -sf $PWD/Secrets/SecretResources/Textures $PWD/Resources/Textures/SecretTextures
 
 echo Done.
