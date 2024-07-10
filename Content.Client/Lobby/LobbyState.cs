ff56d67dc9b5911a6e642948e5ceaa5c72ea4d69
Rain Caldwell <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2024-07-10 10:10:43 +0800

    Change title and make the setup character button actually work

--- Content.Client/Lobby/LobbyState.cs
+++ Content.Client/Lobby/LobbyState.cs
@@ -53,6 +53,7 @@ namespace Content.Client.Lobby
             UpdateLobbyUi();
 
             Lobby.CharacterPreview.CharacterSetupButton.OnPressed += OnSetupPressed;
+            Lobby.SetupCharacterButton.OnPressed += OnSetupPressed;
             Lobby.ReadyButton.OnPressed += OnReadyPressed;
             Lobby.ReadyButton.OnToggled += OnReadyToggled;
 
